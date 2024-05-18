4e08e8944d4396264c624c36856259fddfbd30b4
chandan.gupta@airtel.com <chandan.gupta@airtel.com>
2024-05-18 11:21:59 +0530

    ttest

--- src/main/java/com/lld/java8/lambda/HelloWorld.java
+++ src/main/java/com/lld/java8/lambda/HelloWorld.java
@@ -3,6 +3,8 @@ package com.lld.java8.lambda;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class HelloWorld {
     public static void main(String[] args) {
@@ -63,8 +65,54 @@ class HelloWorld {
         System.out.println("\n" + mergeSortedList);
 
 
-        //11. anagram program
+        //11. total sum of digits
 
+        int total = Stream.of(String.valueOf(1234).split(""))
+                .collect(Collectors.summingInt(Integer::parseInt));
+        System.out.println("\n" + total);
+
+
+        //12. second largest number
+
+        int large = Stream.of(1,5,4,3,2)
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
+        System.out.println("\nLarge : " + large);
+
+        //13 Reverse each of word of a string
+        String reverseStrWords = Arrays.stream(new String("Hello World Test").split(" "))
+                .map((str)-> new StringBuffer(str).reverse().toString())
+                .collect(Collectors.joining(" "));
+
+        System.out.println("\nStr words : " + reverseStrWords);
+
+        //14 Sum of first 10 natural number
+        int rangeSum = IntStream.rangeClosed(1,10).sum();
+        System.out.println("\nSum : " + rangeSum);
+
+
+        //15 Fib series
+        List<Integer> reStream = Stream.iterate(new int[]{0,1}, (f)-> new int[] {f[1], f[0] + f[1]})
+                .limit(10)
+                .map(f-> f[0])
+                .collect(Collectors.toList());
+
+        System.out.println("\nreStream : " + reStream);
+
+        //16. reverse an integer array
+        int[] inp = {2,1,4,5};
+        IntStream.rangeClosed(0, inp.length-1)
+                .map(i-> inp[inp.length-i-1])
+                .toArray();
+
+
+        // 17. palindrome String
+        String sample = "NEEN";
+        Boolean isPalin = IntStream.range(0, sample.length()/2)
+                .allMatch(index -> sample.charAt(index) == sample.charAt(sample.length()-1-index));
+        System.out.println("\nisPalin : " + isPalin);
 
 
     }
